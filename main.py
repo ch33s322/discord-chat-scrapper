062f457f1d0dcf492a3cac289a9b9219465aaac0
ch33s322 <61012984+ch33s322@users.noreply.github.com>
2025-03-17 08:37:37 -0400

    start to compile lists of each reaction

--- main.py
+++ main.py
@@ -24,7 +24,37 @@ async def send_message(message: Message, user_message: str)-> None:
         user_message = user_message[1:]
 
     try:
-        response:str = get_response(user_message)
-        await message.author.send(response) if is_private else message.channel.send(response)
+        response:str = await get_response(user_message, message.channel)
+        if response != '':
+            await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+#handling the startup
+
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running')
+
+#handle incomming messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = message.content
+    channel: str = str(message.channel)
+
+    print(f'[{channel}] {username}: "{user_message}"')
+
+    await send_message(message, user_message)
+
+
+#main entry point
+def main() -> None:
+    client.run(token=TOKEN)
+
+if __name__ == '__main__':
+        print('is this even running?')
+        main()
--- response.py
+++ response.py
@@ -1,2 +1,62 @@
-def get_response(user_input: str) -> str:
-    raise NotImplimentedError('code is missing')
+from random import choice
+
+
+async def scrape(channel) -> None:
+    print(f'scrapping {channel}')
+    rcount = 0
+    lulcount = 0
+    thiscount = 0
+
+    counter = 0
+    #lists
+    significant_messages = list()
+    most_hyperlul = list()
+    most_THIS = list()
+
+
+    def myFunc(e):
+        return e['reactions']
+
+    async for message in channel.history(limit=999999):
+        counter += 1
+        if counter % 100 == 0:
+            print(f'processed {counter} messages... \n{significant_messages.count()} significant messages so far')
+        if message.reactions != list():
+            if message.reactions[0].count >= 1:
+                for react in message.reactions:
+                    if react.emoji.name:
+                        match str(react.emoji.name):
+                            case 'hyperlul':
+                                lulcount += react.count
+                            case 'THIS':
+                                thiscount += react.count
+                    rcount += react.count
+        #put it on a list of interesting things
+        if rcount >= 1:
+            significant_messages.append([{'message_num': counter},  {'reactions': rcount}, {'message_content': message.content}, {'link': message.jump_url}])
+        if lulcount >= 1:
+            most_hyperlul.append([{'message_num': counter},  {'reactions': lulcount}, {'message_content': message.content}, {'link': message.jump_url}])
+        if thiscount >= 1:
+            most_THIS.append([{'message_num': counter},  {'reactions': thiscount}, {'message_content': message.content}, {'link': message.jump_url}])
+        #reset all our iterators
+        rcount = 0
+        lulcount = 0
+        thiscount = 0
+    print((significant_messages[0][0]))                # <class 'list'>    <---- check the data type
+    #significant_messages.sort(key=myFunc)
+    print(f'{counter} messages where processed in {channel}')
+   
+
+
+async def get_response(user_input: str, channel) -> str:
+    lowered: str = user_input.lower()
+
+    if lowered == '!scrape':
+        #start scraping the entire server for info
+        await scrape(channel)
+        return 'channel fully scraped, file name:'
+    else:
+        #do nothing
+        return ''
+    
+
