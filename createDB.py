2a91580e39cb9f96a1c9700f6704d0fbb9b5a143
ch33s322 <61012984+ch33s322@users.noreply.github.com>
2026-07-08 22:05:21 -0400

    schema update

--- createDB.py
+++ createDB.py
@@ -6,44 +6,27 @@ import sqlite3
 def createSchema(conn):
     cursor = conn.cursor()
     #messages table
-    sql = """CREATE TABLE IF NOT EXISTS messages (
-                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+    messagesSQL = """CREATE TABLE IF NOT EXISTS messages (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                mID INTEGER NOT NULL, 
                 userName TEXT NOT NULL,
                 link TEXT NOT NULL,
                 messageContent TEXT NOT NULL,
-                react1 INTEGER,
-                react2 INTEGER,
-                react3 INTEGER,
-                react4 INTEGER,
-                react5 INTEGER,
-                react6 INTEGER,
-                react7 INTEGER,
-                react8 INTEGER,
-                react9 INTEGER,
-                react10 INTEGER,
-                react11 INTEGER,
-                react12 INTEGER,
-                react13 INTEGER,
-                react14 INTEGER,
-                react15 INTEGER,
-                react16 INTEGER,
-                react17 INTEGER,
-                react18 INTEGER,
-                react19 INTEGER,
-                react20 INTEGER,
-                react21 INTEGER,
-                react22 INTEGER,
-                react23 INTEGER,
-                react24 INTEGER,
-                react25 INTEGER,
-                react26 INTEGER,
-                react27 INTEGER,
-                react28 INTEGER,
-                react29 INTEGER,
-                react30 INTEGER
             );"""
-    cursor.execute(sql)
-    print(sql)
+    cursor.execute(messagesSQL)
+    print(messagesSQL)
+
+
+
+    #reacts table
+    reactSQL = """CREATE TABLE IF NOT EXISTS reactions (
+                mID INTEGER NOT NULL, 
+                reactID TEXT NOT NULL,
+                name TEXT NOT NULL,
+                Number INTEGER NOT NULL
+            );"""
+    cursor.execute(reactSQL)
+    print(reactSQL)
 
     # Save changes
     conn.commit()
